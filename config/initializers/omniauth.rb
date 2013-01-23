ec241130e7748cddbe4325cee713eea0f0e35e91
jrlapayne <jrlapayne@gmail.com>
2013-01-22 18:08:00 -0800

    whoops forgot to switch fb auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '282324831851627', 'ce4aaeb4e63d13ed2e6adea6eb354181'  #localhost
-  #provider :facebook, '336449143129017', 'ee6de69f67830a4d75002642f1e8458c'  #limitless-brook-2916
+  #provider :facebook, '282324831851627', 'ce4aaeb4e63d13ed2e6adea6eb354181'  #localhost
+  provider :facebook, '336449143129017', 'ee6de69f67830a4d75002642f1e8458c'  #limitless-brook-2916
 end
